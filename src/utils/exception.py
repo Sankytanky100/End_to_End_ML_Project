2bed1a301ce2aa5e9d7e7555ed41f8c8f6f4477d
Sankytanky100 <140347750+Sankytanky100@users.noreply.github.com>
2024-04-05 18:22:19 -0500

    Model Trainer

--- src/utils/exception.py
+++ src/utils/exception.py
@@ -1,20 +1,27 @@
 import sys
+from src.utils.logger import logging
 
-def error_message_detail(error, error_detail: sys):
-    print("Error detail:", error_detail)  # Print error_detail for debugging
-    _, _, exc_tb = error_detail
-    file_name = exc_tb.tb_frame.f_code.co_filename
-    error_message = "Error occurred in Python script '{0}', line number '{1}', error message: '{2}'".format(
-        file_name, exc_tb.tb_lineno, str(error))
+def error_message_detail(error,error_detail:sys):
+    _,_,exc_tb=error_detail.exc_info()
+    file_name=exc_tb.tb_frame.f_code.co_filename
+    error_message="Error occured in python script name [{0}] line number [{1}] error message[{2}]".format(
+     file_name,exc_tb.tb_lineno,str(error))
+
     return error_message
 
+    
+
 class CustomException(Exception):
-    def __init__(self, error, error_detail: sys):
-        super().__init__(str(error))
-        self.error_message = error_message_detail(error, error_detail=error_detail)
+    def __init__(self,error_message,error_detail:sys):
+        super().__init__(error_message)
+        self.error_message=error_message_detail(error_message,error_detail=error_detail)
     
     def __str__(self):
         return self.error_message
+    
+
+
+        
 
 # Example usage
 #if __name__ == "__main__":
--- src/utils/utils.py
+++ src/utils/utils.py
@@ -4,16 +4,50 @@ import numpy as np
 import pandas as pd
 import dill
 
+from sklearn.metrics import r2_score
+
 from src.utils.exception import CustomException
 
 def save_object(obj, file_path):
+  """Saves a Python object to a file."""
+  try:
+    dir_path = os.path.dirname(file_path)  # Get directory path from file path
+    os.makedirs(dir_path, exist_ok=True)  # Create directory if it doesn't exist
+    with open(file_path, "wb") as file_obj:
+      dill.dump(obj, file_obj)  # Serialize and save object using dill
+  except Exception as e:
+    raise CustomException("Error occurred while saving object", e, sys.exc_info())
+
+
+
+def evaluate_models(X_train, y_train,X_test,y_test,models):
     try:
-        dir_path = os.path.dirname(file_path)
+        report = {}
 
-        os.makedirs(dir_path, exist_ok=True)
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            #para=param[list(models.keys())[i]]
 
-        with open(file_path, "wb") as file_obj:
-            dill.dump(obj, file_obj)
+            #gs = GridSearchCV(model,para,cv=3)
+            #gs.fit(X_train,y_train)
+
+            #model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
+
+            
+            y_train_pred = model.predict(X_train)
+
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+            
+        return report
 
     except Exception as e:
-        raise CustomException("Error occurred while saving object", e, sys.exc_info())
+        raise CustomException(e, sys)
+    
